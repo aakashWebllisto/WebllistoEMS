845b14e30e2ef7baf231746d27be91de5d8a51ad
aakashWebllisto <aakash@webllisto.com>
2022-05-13 12:42:19 +0530

    Template Inheritance implemented

--- webllistoEMS/settings.py
+++ webllistoEMS/settings.py
@@ -68,7 +68,7 @@ EMAIL_HOST_PASSWORD = "webllistoaakash"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
